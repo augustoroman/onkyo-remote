776441082a77ff08690a3aa1472cd61741e6e8fe
Augusto Roman <aroman@vannevartech.com>
2013-11-03 12:48:40 -0800

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,14 @@
+// Package eiscp provides an eISCP (ethernet Integra Serial Control Protocol)
+// implementation.
+//
+// eISCP is a network protocol used by Onkyo Networked Receivers for querying
+// and controlling the device over the network.
+//
+// This package provides generic eISCP support.  The onkyo subpackage provides
+// Onkyo-specific commands and control.
+//
+// For more information, see:
+//   https://sites.google.com/a/webarts.ca/toms-blog/Blog/new-blog-items/javaeiscp-integraserialcontrolprotocol
+//   https://github.com/miracle2k/onkyo-eiscp
+//
+package eiscp
